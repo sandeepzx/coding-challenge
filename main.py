d48c6619fe4b4c876ec83a08d6f29ec5f8d11d39
Sandeep A S <sandeepshine865@gmail.com>
2026-01-04 12:36:51 +0530

    day 2

--- main.py
+++ main.py
@@ -44,3 +44,90 @@ elif(num2 > num1):
     print("second number is greater than first number")
 else:
     print("both are equal")
+
+# Day 2
+
+# task 1
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number:"))
+print("1 for Addition \n2 for Subtraction \n3 for Multiplication \n4 for Division")
+choice = int(input("enter your choice:"))
+result = 0
+
+if(choice == 1):
+    result = num1 + num2
+elif(choice == 2):
+    result = num1 - num2
+elif(choice == 3):
+    result = num1 * num2
+elif(choice == 4):
+    result = num1 * num2
+else:
+    print("enter a valid choice")
+
+print("the result is:",result)
+
+# task 2
+print("1 for Porotta \n2 for Biriyani \n3 for Fried Rice \n4 for Mandhi")
+choice = int(input("enter your choice:"))
+
+match choice:
+    case 1:
+        print("Porotta")
+    case 2:
+        print("Biriyani")
+    case 3:
+        print("Fried Rice")
+    case 4:
+        print("Mandhi")
+    case _:
+        print("Fool")
+
+# task 3
+num1 = int(input("enter a Number:"))
+sum = 0
+for i in range(1,num1):
+    sum += num1
+print("average:",sum/num1)
+
+# task 4
+num1 = int(input("enter a Number:"))
+
+for i in range(1,num1):
+    if (i % 2 == 0):
+        print(i)
+
+# task 5 prime number
+num1 = int(input("enter a Number:"))
+flag = 0
+for i in range(2,num1//2):
+    if (num1 % i == 0):
+        flag = 1
+        break
+if (flag==0):
+    print("prime number")
+else:
+    print("not prime number")
+
+# task 6 pattern
+n = int(input("enter a Number:"))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print("\n")
+
+# task 7
+for i in range(6):
+    print("hi")
+    if(i==3):
+        break
+    print("hello")
+print("finished")
+
+# task 8
+for i in range(6):
+    print("hi")
+    if(i==3):
+        continue
+    print("hello")
+print("finished")
